6a6e00712743790ae84ce96575aa79ee2d493616
Quokka <sm.kim@last30min.com>
2023-07-12 21:59:35 +0900

    로그인시 유저정보 저장 및 예외처리

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { Button, Input } from "antd";
 import { signInWithEmailAndPassword } from "@firebase/auth";
-import { auth } from "@/config/firebase-config";
+import { auth, db } from "@/config/firebase-config";
 import { ChangeEvent, useState } from "react";
 import useStore from "@/store/store";
+import { collection, getDocs } from "@firebase/firestore";
 
 export default function Home() {
   const [email, setEmail] = useState("player2@gmail.com");
   const [password, setPassword] = useState("!12345qwert");
 
-  const userInfo: any = useStore();
+  const userInfoStore: any = useStore();
 
   const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
@@ -21,14 +22,47 @@ export default function Home() {
   const login = async () => {
     try {
       const result = await signInWithEmailAndPassword(auth, email, password);
-      userInfo?.setUid(result?.user?.uid);
-      console.log(result);
-      // location.href = "/list";
+
+      await getUserDataList(result?.user?.uid);
+      userInfoStore?.setUid(result?.user?.uid);
     } catch (error) {
       console.log(error, "error");
       alert("로그인에 실패하였습니다\n아이디와 비밀번호를 확인해주세요");
     }
   };
+
+  const getUserDataList = async (uid: string) => {
+    try {
+      const userInfoList = collection(db, "user");
+      const userDataList = collection(db, "user", uid, "training");
+
+      const userInfo = await getDocs(userInfoList);
+      const userData = await getDocs(userDataList);
+
+      // 로그인한 유저 정보
+      const filteredUserInfo = userInfo?.docs
+        .map((doc) => ({
+          ...doc.data(),
+        }))
+        .filter((data) => data.userUID === uid);
+
+      // 유저 기록 리스트
+      const filteredUserData = userData?.docs?.map((doc) => ({
+        ...doc.data(),
+      }));
+
+      userInfoStore.setUserData(filteredUserData);
+
+      if (!filteredUserInfo?.at(0)?.isTester) {
+        alert("허가된 계정이 아닙니다.\n관리자에게 문의해주세요.");
+      } else {
+        location.href = "/list";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={"min-h-screen flex items-center"}>
       <div className={"px-10 w-full"}>
--- my-app/src/store/store.tsx
+++ my-app/src/store/store.tsx
@@ -4,8 +4,12 @@ export const useStore = create<any>(
   persist(
     (set: any) => ({
       uid: "",
+      userData: [],
       setUid: (state: any) => {
         set(() => ({ uid: state }));
+      },
+      setUserData: (state: any) => {
+        set(() => ({ userData: state }));
       },
     }),
     { name: "uid", storage: createJSONStorage(() => sessionStorage) }
