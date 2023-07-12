51b28ba493ad65c04718a18ed917c15c0d1625f1
Quokka <sm.kim@last30min.com>
2023-07-12 12:39:28 +0900

    firebase 설치

---
+++ my-app/src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaDvwNMO3IdF-T7vIKhErd-cvOIexkuJY",
+  authDomain: "elitestopwatchdevelop.firebaseapp.com",
+  projectId: "elitestopwatchdevelop",
+  storageBucket: "elitestopwatchdevelop.appspot.com",
+  messagingSenderId: "561880562880",
+  appId: "1:561880562880:web:310e45341788e6b5af4125",
+  measurementId: "G-W11503HWEK",
+};
+
+const app = initializeApp(firebaseConfig);
